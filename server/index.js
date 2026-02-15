983b6d2a5da4e10d275e91ee74a837ecc66f7ec9
Sujal <sujalprajapati243@gmail.com>
2026-02-15 02:41:45 -0700

    Form created

--- server/index.js
+++ server/index.js
@@ -4,10 +4,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
 const app = express();
-app.use('/posts', postRoutes);
+
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://spraja13_db_user:Sujal2005@cluster0.ujux8nl.mongodb.net/?appName=Cluster0';
 const PORT = process.env.PORT || 5001;
 mongoose.connect(CONNECTION_URL)
